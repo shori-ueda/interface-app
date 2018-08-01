dbbd96e49f5444bf42db849a1f53e84233dbd24a
上田将理 <ueda.shori@ist.osaka-u.ac.jp>
2018-08-01 16:50:51 +0900

    UIまで作成

---
+++ app/src/main/java/com/example/shoriueda/accessmanagementapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.shoriueda.accessmanagementapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+          startActivity<ScheduleEditActivity>()
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/shoriueda/accessmanagementapp/ScheduleEditActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoriueda.accessmanagementapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_edit)
+    }
+}
